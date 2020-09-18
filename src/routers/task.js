687f137b3761d3dfb12bda20b6a5c80305901572
bikkimahato <bikkimahato123@gmail.com>
2020-09-18 20:27:12 +0530

    Initial commit

---
+++ src/routers/task.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const Task = require('../models/task')
+
+const router = new express.Router()
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find()
+        res.send(tasks)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.patch('/tasks/:id', async (req, res) => {
+    const updatedTask = req.body
+    const updates = Object.keys(updatedTask)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid Update' })
+    }
+    const _id = req.params.id
+    try {
+        const task = await Task.findByIdAndUpdate(_id,
+            updatedTask,
+            { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+        if (!task) {
+            return res.status(404).send('Task not found!')
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+
+module.exports = router
